5bfdcdb4d7e5aa130605af040718d2e76b67381d
node-rookie <749924911@qq.com>
2015-12-04 17:35:15 +0800

    vk

---
+++ test/BotManager.js
@@ -0,0 +1,16 @@
+var BotManagerFactory = require('../index');
+var rabbitmq = require('base-settings').rabbitmq;
+var assert = require('chai').assert;
+var url = 'amqp://' +rabbitmq.username + ':' + rabbitmq.password + '@' + rabbitmq.host + ':' + rabbitmq.port + '/' + rabbitmq.vhost;
+var open = require('amqplib').connect(url);
+
+describe('get bot', function(){
+    it('success get a bot', function(done){
+        var botManagerPromise = BotManagerFactory.create(open);
+        botManagerPromise.then(function(botManager){
+            var bot = botManager.getBot('bot1');
+            assert.equal(bot.id, 'bot1');
+            done();
+        })
+    })
+})
