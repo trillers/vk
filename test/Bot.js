e1bcafbd2dc22087106a93446b8ad5e199557c03
leekangtaqi <leekangtaqi@sina.com>
2015-12-10 22:08:03 +0800

    version 1.1

--- test/Bot.js
+++ test/Bot.js
@@ -1,71 +1,71 @@
-var BrokerFactory = require('vc');
-var BotManagerFactory = require('../index');
-var rabbitmq = require('base-settings').rabbitmq;
-var assert = require('chai').assert;
-var url = 'amqp://' +rabbitmq.username + ':' + rabbitmq.password + '@' + rabbitmq.host + ':' + rabbitmq.port + '/' + rabbitmq.vhost;
-var open = require('amqplib').connect(url);
-
-describe('wenode client test', function(){
-    var brokerPromise = BrokerFactory.create(open, {bot: true});
-    var botManagerPromise = BotManagerFactory.create(open);
-
-    it('success send action msg', function(done){
-        brokerPromise.then(function(broker){
-            var botBroker = broker.getBot();
-            botBroker.onClientAction(function(err, action){
-                assert.ok(action);
-                console.log(action);
-            })
-            botBroker.onClientCommand(function(err, command){
-                assert.ok(command);
-                console.log(command);
-            })
-            botManagerPromise.then(function(botManager){
-                var bot = botManager.getBot('bot1');
-                bot.syncContacts();
-                bot.syncGroups();
-                bot.broadcastImgToContacts(['bu1', 'bu2'], 'mediaId');
-                bot.broadcastImgToGroups(['g1', 'g2'], 'mediaId');
-                bot.broadcastTxtToContacts(['bu1', 'bu2'], 'hello');
-                bot.broadcastTxtToGroups(['g1', 'g2'], 'hello world');
-                bot.sendTxtToContact('bu_test', 'hello');
-                bot.sendTxtToGroup('group1', 'world');
-                bot.sendImgToContact('bu_test', 'mediaId');
-                bot.sendImgToGroup('group1', 'mediaId');
-                bot.profileRequest();
-                bot.start();
-                bot.stop();
-                bot.reload();
-                setTimeout(function(){
-                    done();
-                }, 3000);
-            })
-        });
-    });
-    var ar = [],
-        vcBotBroker = null,
-        vkBroker = null;
-    before(function(done){
-        ar.push(BrokerFactory.create(open, {bot: true}));
-        ar.push(BotManagerFactory.create(open));
-        Promise.all(ar).then(function(arr){
-            vcBotBroker = arr[0].getBot();
-            vkBroker = arr[1].getBot('agent1');
-            done();
-        })
-    });
-    it('receive client action in event', function(done){
-        vkBroker.onClientActionIn(function(err, data){
-            console.log(data);
-            assert.equal(data.foo, 'bar');
-            done()
-        });
-        vcBotBroker.clientActionIn({
-            foo: 'bar'
-        }, 'agent1')
-    })
-});
-
-
-
-
+//var BrokerFactory = require('vc');
+//var BotManagerFactory = require('../index');
+//var rabbitmq = require('base-settings').rabbitmq;
+//var assert = require('chai').assert;
+//var url = 'amqp://' +rabbitmq.username + ':' + rabbitmq.password + '@' + rabbitmq.host + ':' + rabbitmq.port + '/' + rabbitmq.vhost;
+//var open = require('amqplib').connect(url);
+//
+//describe('wenode client test', function(){
+//    var brokerPromise = BrokerFactory.create(open, {bot: true});
+//    var botManagerPromise = BotManagerFactory.create(open);
+//
+//    it('success send action msg', function(done){
+//        brokerPromise.then(function(broker){
+//            var botBroker = broker.getBot();
+//            botBroker.onClientAction(function(err, action){
+//                assert.ok(action);
+//                console.log(action);
+//            })
+//            botBroker.onClientCommand(function(err, command){
+//                assert.ok(command);
+//                console.log(command);
+//            })
+//            botManagerPromise.then(function(botManager){
+//                var bot = botManager.getBot('bot1');
+//                bot.syncContacts();
+//                bot.syncGroups();
+//                bot.broadcastImgToContacts(['bu1', 'bu2'], 'mediaId');
+//                bot.broadcastImgToGroups(['g1', 'g2'], 'mediaId');
+//                bot.broadcastTxtToContacts(['bu1', 'bu2'], 'hello');
+//                bot.broadcastTxtToGroups(['g1', 'g2'], 'hello world');
+//                bot.sendTxtToContact('bu_test', 'hello');
+//                bot.sendTxtToGroup('group1', 'world');
+//                bot.sendImgToContact('bu_test', 'mediaId');
+//                bot.sendImgToGroup('group1', 'mediaId');
+//                bot.profileRequest();
+//                bot.start();
+//                bot.stop();
+//                bot.reload();
+//                setTimeout(function(){
+//                    done();
+//                }, 3000);
+//            })
+//        });
+//    });
+//    var ar = [],
+//        vcBotBroker = null,
+//        vkBroker = null;
+//    before(function(done){
+//        ar.push(BrokerFactory.create(open, {bot: true}));
+//        ar.push(BotManagerFactory.create(open));
+//        Promise.all(ar).then(function(arr){
+//            vcBotBroker = arr[0].getBot();
+//            vkBroker = arr[1].getBot('agent1');
+//            done();
+//        })
+//    });
+//    it('receive client action in event', function(done){
+//        vkBroker.onClientActionIn(function(err, data){
+//            console.log(data);
+//            assert.equal(data.foo, 'bar');
+//            done()
+//        });
+//        vcBotBroker.clientActionIn({
+//            foo: 'bar'
+//        }, 'agent1')
+//    })
+//});
+//
+//
+//
+//
--- test/funcs.js
+++ test/funcs.js
@@ -1,4 +1,3 @@
-var BrokerFactory = require('vc');
 var BotManagerFactory = require('../index');
 var rabbitmq = require('base-settings').rabbitmq;
 var assert = require('chai').assert;
@@ -9,20 +8,116 @@ describe.only('vk funcs', function(){
     var ar = [],
         api = null;
     before(function(done){
-        ar.push(BrokerFactory.create(open, {bot: true}));
         ar.push(BotManagerFactory.create(open));
         Promise.all(ar).then(function(arr){
-            api = arr[1].getBot('agent1');
+            api = arr[0].getBot('agent1');
             done();
         })
     });
-    it('#start', function(done){
-        console.log('begin')
-        api.start();
+    afterEach(function(){
+        api.offClientActionIn();
+    });
+    /**
+     * commands
+     */
+    it('#start, intent to register and login', function(done){
+        console.log('begin');
         api.onClientActionIn(function(err, data){
+            assert.equal('need-login', data.Action);
+            assert.equal('agent1', data.AgentId);
+            //TODO call customer to login
+            done();
+        });
+        api.start({intention: 'register', mode: 'untrusted'});
+    });
+    //
+    ///**
+    // * passive events
+    // */
+    it('#receive first-profile event', function(done){
+        console.log('begin');
+        api.onClientActionIn(function(err, data){
+            assert.equal('first-profile', data.Action);
+            assert.equal('agent1', data.AgentId);
+            done();
+        });
+    });
+
+    /**
+     * active events
+     */
+    it('#request profile ', function(done){
+        api.onClientActionIn(function(err, data){
+            assert.equal('profile-request', data.Action);
             console.log(data);
-        })
-    })
+            done();
+        });
+        api.profileRequest('独自等待');
+    });
+    it('#group list', function(done){
+        api.onClientActionIn(function(err, data){
+            assert.equal('sync-groups', data.Action);
+            console.log(data);
+            done();
+        });
+        api.syncGroups();
+    });
+    //it('#contact list', function(done){
+    //    api.onClientActionIn(function(err, data){
+    //        assert.equal('remark-contact', data.Action);
+    //        console.log(data);
+    //        done();
+    //    });
+    //    api.syncContacts();
+    //});
+    it('#send txt to a group', function(done){
+        api.sendTxtToContact('独自等待', '单条个人文本消息');
+        setTimeout(function(){
+            done();
+        }, 2000);
+    });
+    it('#send a image to a contact', function(done){
+        api.sendImgToContact('独自等待', '/Users/bjhl/dev/codebase/vb/public/geek.png');
+        setTimeout(function(){
+            done();
+        }, 2000);
+    });
+    it('#send txt to a group', function(done){
+        api.sendTxtToGroup('外挂者', '单条群文本消息');
+        setTimeout(function(){
+            done();
+        }, 2000);
+    });
+    it('#send image to a group', function(done){
+        api.sendImgToGroup('外挂者', '/Users/bjhl/dev/codebase/vb/public/geek.png');
+        setTimeout(function(){
+            done();
+        }, 2000);
+    });
+    it('#send a txt to contacts', function(done){
+        api.broadcastTxtToContacts(['独自等待', '独自等待'], '群发个人消息');
+        setTimeout(function(){
+            done();
+        }, 2000);
+    });
+    it('#send a txt to a group', function(done){
+        api.broadcastImgToContacts(['独自等待', '独自等待'], '/Users/bjhl/dev/codebase/vb/public/geek.png');
+        setTimeout(function(){
+            done();
+        }, 2000);
+    });
+    it('#send a txt to a group', function(done){
+        api.broadcastTxtToGroups(['外挂者', '外挂者'], '群发群消息');
+        setTimeout(function(){
+            done();
+        }, 2000);
+    });
+    it('#send a image to groups', function(done){
+        api.broadcastImgToGroups(['外挂者', '外挂者'], '/Users/bjhl/dev/codebase/vb/public/geek.png');
+        setTimeout(function(){
+            done();
+        }, 2000);
+    });
 });
 
 
